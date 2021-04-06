7aca8eb20babf872b21b6525a06f06bdcb312f9d
Ahmed <ahmedi3aaa@hotmail.com>
2021-04-06 16:38:58 +0200

    fixed some formatting

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -1,16 +1,16 @@
-import java.util. * ;
-import java.sql. * ;
+import java.util.*;
+import java.sql.*;
 
 /**
-* Class responsible for getting info from the database and
-* editing the database
-* @author Ahmed Abdullah
-* @author Dong Wook Son
-* @author Jonathan Chong
-* @author Ahmed Abbas
-* @version 1.5
-* @since 1.0
-*/
+ * Class responsible for getting info from the database and
+ * editing the database
+ * @author Ahmed Abdullah
+ * @author Dong Wook Son
+ * @author Jonathan Chong
+ * @author Ahmed Abbas
+ * @version 1.5
+ * @since 1.0
+ */
 public class DatabaseConnection {
 	private final String DBURL;
 	private final String USERNAME;
@@ -19,82 +19,81 @@ public class DatabaseConnection {
 	private ResultSet queryResults;
 	private Statement myStatment;
 	private PreparedStatement myPreparedStatment;
-	private ArrayList < String > columns;
+	private ArrayList<String> columns;
 	private int rows;
 
 	/**
-         * A constructor, stores connection data as member variables and
-         * initializes connection with the database
-         * @param DBURL URL of the desired inventory database
-         * @param USERNAME username of the local database
-         * @param PASSWORD password of the local database
-         */
+	 * A constructor, stores connection data as member variables and
+	 * initializes connection with the database
+	 * @param DBURL URL of the desired inventory database
+	 * @param USERNAME username of the local database
+	 * @param PASSWORD password of the local database
+	 */
 	DatabaseConnection(String DBURL, String USERNAME, String PASSWORD) {
 		this.DBURL = DBURL;
 		this.USERNAME = USERNAME;
 		this.PASSWORD = PASSWORD;
 		try {
 			initializeConnection();
-		}
-		catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
 	}
 
 	/**
-         * Gets the stored database URL
-         * @return A String containing the stored database URL
-         */
+	 * Gets the stored database URL
+	 * @return A String containing the stored database URL
+	 */
 	public String getDburl() {
 		return this.DBURL;
 	}
 
 	/**
-         * Gets the stored username used to access database 
-         * @return A String containing the stored username
-         */
+	 * Gets the stored username used to access database 
+	 * @return A String containing the stored username
+	 */
 	public String getUsername() {
 		return this.USERNAME;
 	}
 
 	/**
-         * Gets the stored password used to access database 
-         * @return A String containing the stored password
-         */
+	 * Gets the stored password used to access database 
+	 * @return A String containing the stored password
+	 */
 	public String getPassword() {
 		return this.PASSWORD;
 	}
 
 	/**
-         * Gets the current connection 
-         * @return An Object representing the current connection
-         */
+	 * Gets the current connection 
+	 * @return An Object representing the current connection
+	 */
 	public Connection getDatabaseConnection() {
 		return this.databaseConnection;
 	}
 
 	/**
-         * Gets the number of rows of the table being searched
-         * @return An int representing the current table's number of rows
-         */
+	 * Gets the number of rows of the table being searched
+	 * @return An int representing the current table's number of rows
+	 */
 	public int getRows() {
 		return this.rows;
 	}
 
 	/**
-         * Returns the names of all the columns in the table being searched
-         * @return An ArrayList containing the header names of the columns
-         * of the current table
-         */
-	public ArrayList < String > getColumns() {
+	 * Returns the names of all the columns in the table being searched
+	 * @return An ArrayList containing the header names of the columns
+	 * of the current table
+	 */
+	public ArrayList<String> getColumns() {
 		return this.columns;
 	}
 
 	/**
-         * Initializes the connection with the local database using the passed username,
-         * password and database URL
-         */
+	 * Initializes the connection with the local database using the passed username,
+	 * password and database URL
+	 */
 	private
 	void initializeConnection() throws SQLException {
 		// Setup connection with database
@@ -102,29 +101,24 @@ public class DatabaseConnection {
 	}
 
 	/**
-         * Given a table name and a type of furniture, it populates an ArrayList
-         * with column names of the current table and populates an ArrayList of HashMaps with
-         * the content of all columns in a database for items which match the given type in 
-         * the given table
-         * @param tableName name of the table to search for data in the database
-         * @param type type of furniture to search for in the given table
-         * @return An ArrayList of HashMaps with String keys and String values representing
-         * the column name and corresponding data in the data base for each row in the given table
-         * that matches the given type
-         */
-	public ArrayList < HashMap < String,
-	String >> retrieveData(String tableName, String type) {
+	 * Given a table name and a type of furniture, it populates an ArrayList
+	 * with column names of the current table and populates an ArrayList of HashMaps with
+	 * the content of all columns in a database for items which match the given type in 
+	 * the given table
+	 * @param tableName name of the table to search for data in the database
+	 * @param type type of furniture to search for in the given table
+	 * @return An ArrayList of HashMaps with String keys and String values representing
+	 * the column name and corresponding data in the data base for each row in the given table
+	 * that matches the given type
+	 */
+	public ArrayList<HashMap<String, String>> retrieveData(String tableName, String type) {
 
-		ArrayList < HashMap < String,
-		String >> furniture = null;
+		ArrayList<HashMap<String, String>> furniture = null;
 		try {
 
-			ArrayList < String > columns = new ArrayList < String > ();
-			furniture = new ArrayList < HashMap < String,
-			String >> ();
-			HashMap < String,
-			String > parts = new HashMap < String,
-			String > ();
+			ArrayList<String> columns = new ArrayList<String> ();
+			furniture = new ArrayList<HashMap<String, String>> ();
+			HashMap<String, String> parts = new HashMap<String, String> ();
 
 			// Get column names
 			myStatment = databaseConnection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
@@ -160,8 +154,7 @@ public class DatabaseConnection {
 			// Inserting all furnitures retrieved into furniture ArrayList
 			// Each element in ArrayList is a HashMap that contains the column name and the value.
 			while (queryResults.next()) {
-				parts = new HashMap < String,
-				String > ();
+				parts = new HashMap<String, String> ();
 
 				for (String column: columns) {
 					// Populating HashMap
@@ -175,24 +168,23 @@ public class DatabaseConnection {
 			this.rows = rows;
 
 			myStatment.close();
-		} catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return furniture;
 	}
 
 	/**
-         * Given a table and an item type, returns a List of Maps having a String key and String value
-         * representing a possible furniture manufacturer where the data of each manufacturer are stored
-         * as key value pairs
-         * @param itemTable table name to search for item
-         * @return List of possible manufacturers of the given item stored as key-value pairs
-         * in a Map
-         */
-	public ArrayList < HashMap < String,
-	String >> getPossibleManufacturer(String itemTable) {
+	 * Given a table and an item type, returns a List of Maps having a String key and String value
+	 * representing a possible furniture manufacturer where the data of each manufacturer are stored
+	 * as key value pairs
+	 * @param itemTable table name to search for item
+	 * @return List of possible manufacturers of the given item stored as key-value pairs
+	 * in a Map
+	 */
+	public ArrayList<HashMap<String, String>> getPossibleManufacturer(String itemTable) {
 
-		HashSet < String > possibleManufacturers = new HashSet < String > ();
+		HashSet<String> possibleManufacturers = new HashSet<String> ();
 
 		String result[] = null;
 		try {
@@ -213,21 +205,18 @@ public class DatabaseConnection {
 				result = new String[possibleManufacturers.size()];
 				possibleManufacturers.toArray(result);
 			}
-		} catch(SQLSyntaxErrorException ex) {
+		} catch (SQLSyntaxErrorException ex) {
 			return null;
-		} catch(SQLException ex) {
+		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
 
 		try {
-			ArrayList < HashMap < String,
-			String >> queryResultList = new ArrayList < HashMap < String,
-			String >> ();
-			HashMap < String,
-			String > entry = null;
+			ArrayList<HashMap<String, String>> queryResultList = new ArrayList<HashMap<String, String>> ();
+			HashMap<String, String> entry = null;
 
 			// for each manufacturer id, get all data associated with id and store it in a HashMap
-			for (int i = 0; i < result.length; i++) {
+			for (int i = 0; i<result.length; i++) {
 				myStatment = databaseConnection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
 				queryResults = myStatment.executeQuery("SELECT * FROM MANUFACTURER WHERE ManuID = '" + result[i] + "'");
 
@@ -239,10 +228,9 @@ public class DatabaseConnection {
 					ResultSetMetaData metaData = queryResults.getMetaData();
 					Integer columnCount = metaData.getColumnCount();
 
-					entry = new HashMap < String,
-					String > ();
+					entry = new HashMap<String, String> ();
 					// add all data in the row of the manufaturer with the current id in a HashMap
-					for (int j = 1; j <= columnCount.intValue(); j++) {
+					for (int j = 1; j<= columnCount.intValue(); j++) {
 						entry.put(metaData.getColumnName(j), queryResults.getObject(j).toString());
 					}
 					queryResultList.add(entry);
@@ -250,7 +238,7 @@ public class DatabaseConnection {
 			}
 			myStatment.close();
 			return queryResultList;
-		} catch(SQLException ex) {
+		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
 
@@ -258,14 +246,14 @@ public class DatabaseConnection {
 	}
 
 	/**
-         * Given an array of IDs and a table, deletes these IDs from the table
-         * @param id An array containing the IDs to be deleted
-         * @param itemTable the table from which the IDs should be deleted
-         */
+	 * Given an array of IDs and a table, deletes these IDs from the table
+	 * @param id An array containing the IDs to be deleted
+	 * @param itemTable the table from which the IDs should be deleted
+	 */
 	public void deleteUsedItems(String[] id, String itemTable) {
 
 		// delete each of the given IDs
-		for (int i = 0; i < id.length; i++) {
+		for (int i = 0; i<id.length; i++) {
 
 			try {
 				String query = "DELETE FROM " + itemTable + " WHERE ID = ?";
@@ -276,15 +264,15 @@ public class DatabaseConnection {
 				int rowCount = myPreparedStatment.executeUpdate();
 				System.out.println("Rows deleted: " + rowCount);
 				myPreparedStatment.close();
-			} catch(SQLException ex) {
+			} catch (SQLException ex) {
 				ex.printStackTrace();
 			}
 		}
 	}
 
 	/**
-         * closes ResultSet ana Connection to the database
-         */
+	 * closes ResultSet ana Connection to the database
+	 */
 	public void close() {
 		try {
 			if (queryResults != null) {
@@ -294,7 +282,7 @@ public class DatabaseConnection {
 				databaseConnection.close();
 			}
 
-		} catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
