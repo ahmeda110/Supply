6b29f801f9600f27eeb33281a2ef79da76e81461
Ahmed <ahmedi3aaa@hotmail.com>
2021-04-06 16:28:05 +0200

    database tests and extra documentation

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -1,7 +1,5 @@
-package FinalProject;
-
-import java.util.*;
-import java.sql.*;
+import java.util. * ;
+import java.sql. * ;
 
 /**
 * Class responsible for getting info from the database and
@@ -21,10 +19,10 @@ public class DatabaseConnection {
 	private ResultSet queryResults;
 	private Statement myStatment;
 	private PreparedStatement myPreparedStatment;
-	private ArrayList<String> columns;
+	private ArrayList < String > columns;
 	private int rows;
-        
-        /**
+
+	/**
          * A constructor, stores connection data as member variables and
          * initializes connection with the database
          * @param DBURL URL of the desired inventory database
@@ -35,16 +33,16 @@ public class DatabaseConnection {
 		this.DBURL = DBURL;
 		this.USERNAME = USERNAME;
 		this.PASSWORD = PASSWORD;
-                try{
-                  initializeConnection();
-                }
-                catch (SQLException e) {
-                    e.printStackTrace();
+		try {
+			initializeConnection();
 		}
-		
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
-        
-        /**
+
+	/**
          * Gets the stored database URL
          * @return A String containing the stored database URL
          */
@@ -52,57 +50,58 @@ public class DatabaseConnection {
 		return this.DBURL;
 	}
 
-        /**
+	/**
          * Gets the stored username used to access database 
          * @return A String containing the stored username
          */
 	public String getUsername() {
 		return this.USERNAME;
 	}
-        
-        /**
+
+	/**
          * Gets the stored password used to access database 
          * @return A String containing the stored password
          */
 	public String getPassword() {
 		return this.PASSWORD;
 	}
-        
-        /**
+
+	/**
          * Gets the current connection 
          * @return An Object representing the current connection
          */
 	public Connection getDatabaseConnection() {
 		return this.databaseConnection;
 	}
-        
-        /**
+
+	/**
          * Gets the number of rows of the table being searched
          * @return An int representing the current table's number of rows
          */
 	public int getRows() {
 		return this.rows;
 	}
-        
-        /**
+
+	/**
          * Returns the names of all the columns in the table being searched
          * @return An ArrayList containing the header names of the columns
          * of the current table
          */
-	public ArrayList<String> getColumns() {
+	public ArrayList < String > getColumns() {
 		return this.columns;
 	}
-        
-        /**
+
+	/**
          * Initializes the connection with the local database using the passed username,
          * password and database URL
          */
-	private void initializeConnection() throws SQLException {
-            // Setup connection with database
-            databaseConnection = DriverManager.getConnection(this.DBURL, this.USERNAME, this.PASSWORD);
-        }
-        
-        /**
+	private
+	void initializeConnection() throws SQLException {
+		// Setup connection with database
+		databaseConnection = DriverManager.getConnection(this.DBURL, this.USERNAME, this.PASSWORD);
+	}
+
+	/**
          * Given a table name and a type of furniture, it populates an ArrayList
          * with column names of the current table and populates an ArrayList of HashMaps with
          * the content of all columns in a database for items which match the given type in 
@@ -113,20 +112,25 @@ public class DatabaseConnection {
          * the column name and corresponding data in the data base for each row in the given table
          * that matches the given type
          */
-	public ArrayList<HashMap<String, String>> retrieveData(String tableName, String type) {
+	public ArrayList < HashMap < String,
+	String >> retrieveData(String tableName, String type) {
 
-		ArrayList<HashMap<String, String>> furniture = null;
+		ArrayList < HashMap < String,
+		String >> furniture = null;
 		try {
 
-			ArrayList<String> columns = new ArrayList<String> ();
-			furniture = new ArrayList<HashMap<String, String>> ();
-			HashMap<String, String> parts = new HashMap<String, String> ();
+			ArrayList < String > columns = new ArrayList < String > ();
+			furniture = new ArrayList < HashMap < String,
+			String >> ();
+			HashMap < String,
+			String > parts = new HashMap < String,
+			String > ();
 
 			// Get column names
 			myStatment = databaseConnection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-			queryResults = myStatment.executeQuery("SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`= 'inventory' AND `TABLE_NAME` = '" + tableName + "'");
+			queryResults = myStatment.executeQuery("SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` " + "WHERE `TABLE_SCHEMA`= 'inventory' AND `TABLE_NAME` = '" + tableName + "'");
 
-                        // if tablename is not available in the invntory database
+			// if tablename is not available in the invntory database
 			if (queryResults.next() == false) {
 				myStatment.close();
 				return null;
@@ -140,16 +144,24 @@ public class DatabaseConnection {
 
 			this.columns = columns;
 
-			myStatment = databaseConnection.createStatement();
+			myStatment = databaseConnection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
 			queryResults = myStatment.executeQuery("SELECT * FROM " + tableName + " WHERE Type = \"" + type + "\"");
 
 			// Need this number to terminate recursive function.
 			int rows = 0;
 
+			if (queryResults.next() == false) {
+				myStatment.close();
+				return null;
+			}
+
+			queryResults.previous(); //go back to first result object
+
 			// Inserting all furnitures retrieved into furniture ArrayList
 			// Each element in ArrayList is a HashMap that contains the column name and the value.
 			while (queryResults.next()) {
-				parts = new HashMap<String, String> ();
+				parts = new HashMap < String,
+				String > ();
 
 				for (String column: columns) {
 					// Populating HashMap
@@ -163,58 +175,63 @@ public class DatabaseConnection {
 			this.rows = rows;
 
 			myStatment.close();
-		} catch (SQLException e) {
+		} catch(SQLException e) {
 			e.printStackTrace();
 		}
 		return furniture;
 	}
 
-        /**
+	/**
          * Given a table and an item type, returns a List of Maps having a String key and String value
          * representing a possible furniture manufacturer where the data of each manufacturer are stored
          * as key value pairs
          * @param itemTable table name to search for item
-         * @param itemType item type to search for in the given table
          * @return List of possible manufacturers of the given item stored as key-value pairs
          * in a Map
          */
-	public ArrayList<HashMap<String, Object>> getPossibleManufacturer(String itemTable, String itemType) {
+	public ArrayList < HashMap < String,
+	String >> getPossibleManufacturer(String itemTable) {
 
-		HashSet<String> possibleManufacturers = new HashSet<String> ();
+		HashSet < String > possibleManufacturers = new HashSet < String > ();
 
 		String result[] = null;
 		try {
 			myStatment = databaseConnection.createStatement();
 			queryResults = myStatment.executeQuery("SELECT * FROM " + itemTable);
-                        
-                        // if itemTable is not available in inventory
+
+			// if itemTable is not available in inventory
 			if (queryResults.next() == false) {
 				myStatment.close();
 				return null;
 			} else {
-                                // get all manufacturers of current table
+				// get all manufacturers of current table
 				possibleManufacturers.add(queryResults.getString("ManuID")); //add without calling next(), already
-				while (queryResults.next()) {                                   //called in if statment
+				while (queryResults.next()) { //called in if statment
 					possibleManufacturers.add(queryResults.getString("ManuID"));
 				}
 				myStatment.close();
 				result = new String[possibleManufacturers.size()];
 				possibleManufacturers.toArray(result);
 			}
-		} catch (SQLException ex) {
+		} catch(SQLSyntaxErrorException ex) {
+			return null;
+		} catch(SQLException ex) {
 			ex.printStackTrace();
 		}
 
 		try {
-			ArrayList<HashMap<String, Object>> queryResultList = new ArrayList<HashMap<String, Object>> ();
-			HashMap<String, Object> entry = null;
+			ArrayList < HashMap < String,
+			String >> queryResultList = new ArrayList < HashMap < String,
+			String >> ();
+			HashMap < String,
+			String > entry = null;
 
-                        // for each manufacturer id, get all data associated with id and store it in a HashMap
-			for (int i = 0; i<result.length; i++) {
+			// for each manufacturer id, get all data associated with id and store it in a HashMap
+			for (int i = 0; i < result.length; i++) {
 				myStatment = databaseConnection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
 				queryResults = myStatment.executeQuery("SELECT * FROM MANUFACTURER WHERE ManuID = '" + result[i] + "'");
-                                
-                                // if id not available in manufacturers table
+
+				// if id not available in manufacturers table
 				if (queryResults.next() == false) {
 					myStatment.close();
 					return queryResultList;
@@ -222,31 +239,32 @@ public class DatabaseConnection {
 					ResultSetMetaData metaData = queryResults.getMetaData();
 					Integer columnCount = metaData.getColumnCount();
 
-					entry = new HashMap<String,Object> ();
-                                        // add all data in the row of the manufaturer with the current id in a HashMap
-					for (int j = 1; j<= columnCount.intValue(); j++) {
-						entry.put(metaData.getColumnName(j), queryResults.getObject(j));
+					entry = new HashMap < String,
+					String > ();
+					// add all data in the row of the manufaturer with the current id in a HashMap
+					for (int j = 1; j <= columnCount.intValue(); j++) {
+						entry.put(metaData.getColumnName(j), queryResults.getObject(j).toString());
 					}
 					queryResultList.add(entry);
 				}
 			}
 			myStatment.close();
 			return queryResultList;
-		} catch (SQLException ex) {
+		} catch(SQLException ex) {
 			ex.printStackTrace();
 		}
 
 		return null;
 	}
 
-        /**
+	/**
          * Given an array of IDs and a table, deletes these IDs from the table
          * @param id An array containing the IDs to be deleted
          * @param itemTable the table from which the IDs should be deleted
          */
 	public void deleteUsedItems(String[] id, String itemTable) {
-                
-                // delete each of the given IDs
+
+		// delete each of the given IDs
 		for (int i = 0; i < id.length; i++) {
 
 			try {
@@ -258,20 +276,25 @@ public class DatabaseConnection {
 				int rowCount = myPreparedStatment.executeUpdate();
 				System.out.println("Rows deleted: " + rowCount);
 				myPreparedStatment.close();
-			} catch (SQLException ex) {
+			} catch(SQLException ex) {
 				ex.printStackTrace();
 			}
 		}
 	}
-        
-        /**
+
+	/**
          * closes ResultSet ana Connection to the database
          */
 	public void close() {
 		try {
-			queryResults.close();
-			databaseConnection.close();
-		} catch (SQLException e) {
+			if (queryResults != null) {
+				queryResults.close();
+			}
+			if (databaseConnection != null) {
+				databaseConnection.close();
+			}
+
+		} catch(SQLException e) {
 			e.printStackTrace();
 		}
 	}
---
+++ DatabaseConnectionTest.java
@@ -0,0 +1,355 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.io.*;
+import java.util.Collections;
+
+/**
+ * Class responsible for testing the DataBaseConnection
+ * class methods
+ * @author Ahmed Abdullah
+ * @author Dong Wook Son
+ * @author Jonathan Chong
+ * @author Ahmed Abbas
+ * @version 1.1
+ * @since 1.0
+ */
+public class DatabaseConnectionTest {
+	private static DatabaseConnection instance = null;
+	private static final String USERNAME = "Ahmed"; //--------------!important----------------
+	private static final String PASSWORD = "ensf409"; // CHANGE TO YOUR OWN DATABASE USERNAME AND PASSWORD
+
+	public DatabaseConnectionTest() {}
+
+	@BeforeClass
+	public static void setUpClass() {
+		System.out.println("\n\n--------------------------------------------");
+		System.out.println("Resetting Local Database Before Starting Tests.");
+		System.out.println("Please Wait..............");
+		System.out.println("--------------------------------------------\n\n");
+		Connection testConnection = null;
+		try {
+			testConnection = DriverManager.getConnection("jdbc:mysql://localhost", USERNAME, PASSWORD);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		BufferedReader input = null;
+		try { //--------------!important----------------
+			input = new BufferedReader(new FileReader("./inventory.sql")); //inventory file must be in the 
+		} //directory from which program was run
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		Statement myStatment = null;
+		String currentLine = "";
+		StringBuilder result = new StringBuilder();
+		try {
+			myStatment = testConnection.createStatement();
+
+			while ((currentLine = input.readLine()) != null) {
+				result.append(currentLine.trim());
+			}
+			String[] executable = result.toString().replace("\n", "").split(";");
+
+			for (String
+				var: executable) {
+				myStatment.execute(var);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (myStatment != null) {
+				try {
+					myStatment.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		//instance.close();
+	}
+
+	@Before
+	public void setUp() {
+		// creating an instance for each class to remove stored member variables 
+		// from previous tests
+		instance = new DatabaseConnection("jdbc:mysql://localhost/inventory", USERNAME, PASSWORD);
+	}
+
+	@After
+	public void tearDown() {
+		// close ResultSet and Connection after using it
+		instance.close();
+	}
+
+	/**
+	 * Test of getDburl method, of class DatabaseConnection.
+	 */
+	@Test
+	public void testGetDburl() {
+		System.out.println("getDburl Method Test....");
+		String expResult = "jdbc:mysql://localhost/inventory";
+		String result = instance.getDburl();
+		assertEquals("returned database url didn't match expected url", expResult, result);
+	}
+
+	/**
+	 * Test of getUsername method, of class DatabaseConnection.
+	 */
+	@Test
+	public void testGetUsername() {
+		System.out.println("getUsername Method Test....");
+		String expResult = USERNAME;
+		String result = instance.getUsername();
+		assertEquals("returned username didn't match expected username", expResult, result);
+	}
+
+	/**
+	 * Test of getPassword method, of class DatabaseConnection.
+	 */
+	@Test
+	public void testGetPassword() {
+		System.out.println("getPassword Method Test....");
+		String expResult = PASSWORD;
+		String result = instance.getPassword();
+		assertEquals("returned password didn't match expected password", expResult, result);
+	}
+
+	/**
+	 * Test of retrieveData method, of class DatabaseConnection.
+	 */
+	@Test
+	public void testRetrieveDataValidData() {
+		System.out.println("retrieveData Method With Valid Data Test....");
+		String tableName = "chair";
+		String type = "Kneeling";
+
+		HashMap<String, String> entryOne = new HashMap<String, String> ();
+		entryOne.put("ID", "C1320");
+		entryOne.put("Type", "Kneeling");
+		entryOne.put("Legs", "Y");
+		entryOne.put("Arms", "N");
+		entryOne.put("Seat", "N");
+		entryOne.put("Cushion", "N");
+		entryOne.put("Price", "50");
+		entryOne.put("ManuID", "002");
+
+		HashMap<String, String> entryTwo = new HashMap<String, String> ();
+		entryTwo.put("ID", "C3819");
+		entryTwo.put("Type", "Kneeling");
+		entryTwo.put("Legs", "N");
+		entryTwo.put("Arms", "N");
+		entryTwo.put("Seat", "Y");
+		entryTwo.put("Cushion", "N");
+		entryTwo.put("Price", "75");
+		entryTwo.put("ManuID", "005");
+
+		ArrayList<HashMap<String, String>> expResult = new ArrayList<HashMap<String, String>> ();
+		expResult.add(entryOne);
+		expResult.add(entryTwo);
+		ArrayList<HashMap<String, String>> result = instance.retrieveData(tableName, type);
+		assertEquals(expResult, result);
+	}
+
+	/**
+	 * Test of retrieveData method, of class DatabaseConnection.
+	 */
+	@Test
+	public void testRetrieveDataInvalidTable() {
+		System.out.println("retrieveData Method With Invalid TableName Test....");
+		String tableName = "chairrr";
+		String type = "Kneeling";
+
+		ArrayList<HashMap<String, String>> expResult = null;
+
+		ArrayList<HashMap<String, String>> result = instance.retrieveData(tableName, type);
+		assertEquals(expResult, result);
+	}
+
+	/**
+	 * Test of retrieveData method, of class DatabaseConnection.
+	 */
+	@Test
+	public void testRetrieveDataInvalidType() {
+		System.out.println("retrieveData Method With Invalid Item Type Test....");
+		String tableName = "chair";
+		String type = "invalid";
+
+		ArrayList<HashMap<String, String>> expResult = null;
+
+		ArrayList<HashMap<String, String>> result = instance.retrieveData(tableName, type);
+		assertEquals(expResult, result);
+	}
+
+	/**
+	 * Test of getRows method, of class DatabaseConnection.
+	 */
+	@Test
+	public void testGetRowsValidData() {
+		System.out.println("getRows Method With Valid Data....");
+		String tableName = "Lamp";
+		String type = "Desk";
+		ArrayList<HashMap<String, String>> temp = instance.retrieveData(tableName, type);
+		int expResult = 7;
+		int result = instance.getRows();
+		assertEquals(expResult, result);
+	}
+
+	@Test
+	public void testGetRowsInValidData() {
+		System.out.println("getRows Method With Invalid Data....");
+		String tableName = "Lampp";
+		String type = "Deskkk";
+		instance.retrieveData(tableName, type);
+		int expResult = 0;
+		int result = instance.getRows();
+		assertEquals(expResult, result);
+	}
+
+	/**
+	 * Test of getColumns method, of class DatabaseConnection.
+	 */
+	@Test
+	public void testGetColumnsValidData() {
+		System.out.println("getColumns Method With Valid Data....");
+		String tableName = "filing";
+		String type = "small";
+		instance.retrieveData(tableName, type);
+		ArrayList<String> expResult = new ArrayList<String> ();
+		String[] resultArray = {
+			"ID", "Type", "Rails", "Drawers",
+			"Cabinet", "Price", "ManuID"
+		};
+
+		Collections.addAll(expResult, resultArray);
+		ArrayList<String> result = instance.getColumns();
+		assertEquals(expResult, result);
+	}
+
+	@Test
+	public void testGetColumnsInValidData() {
+		System.out.println("getColumns Method With InValid Data....");
+		String tableName = "notvalid";
+		String type = "invalid";
+		instance.retrieveData(tableName, type);
+
+		ArrayList<String> expResult = null;
+
+		ArrayList<String> result = instance.getColumns();
+		assertEquals(expResult, result);
+	}
+
+	/**
+	 * Test of getPossibleManufacturer method, of class DatabaseConnection.
+	 */
+	@Test
+	public void testGetPossibleManufacturerValidData() {
+		System.out.println("getPossibleManufacturer Method With Valid Data");
+		String itemTable = "Lamp";
+
+		HashMap<String, String> entryOne = new HashMap<String, String> ();
+		entryOne.put("ManuID", "005");
+		entryOne.put("Name", "Fine Office Supplies");
+		entryOne.put("Phone", "403-980-9876");
+		entryOne.put("Province", "AB");
+
+		HashMap<String, String> entryTwo = new HashMap<String, String> ();
+		entryTwo.put("ManuID", "004");
+		entryTwo.put("Name", "Furniture Goods");
+		entryTwo.put("Phone", "306-512-5508");
+		entryTwo.put("Province", "SK");
+
+		HashMap<String, String> entryThree = new HashMap<String, String> ();
+		entryThree.put("ManuID", "002");
+		entryThree.put("Name", "Office Furnishings");
+		entryThree.put("Phone", "587-890-4387");
+		entryThree.put("Province", "AB");
+
+		ArrayList<HashMap<String, String>> expResult = new ArrayList<HashMap<String, String>> ();
+		expResult.add(entryOne);
+		expResult.add(entryTwo);
+		expResult.add(entryThree);
+
+		ArrayList<HashMap<String, String>> result = instance.getPossibleManufacturer(itemTable);
+		assertTrue(expResult.size() == result.size() && expResult.containsAll(result) && result.containsAll(expResult));
+	}
+
+	/**
+	 * Test of getPossibleManufacturer method, of class DatabaseConnection.
+	 */
+	@Test
+	public void testGetPossibleManufacturerInValidData() {
+		System.out.println("getPossibleManufacturer Method With Invalid Data");
+		String itemTable = "invalid";
+
+		ArrayList<HashMap<String, String>> expResult = null;
+
+		ArrayList<HashMap<String, String>> result = instance.getPossibleManufacturer(itemTable);
+		assertEquals(expResult, result);
+	}
+
+	/**
+	 * Test of deleteUsedItems method, of class DatabaseConnection.
+	 */
+	@Test
+	public void testDeleteUsedItems() {
+		System.out.println("deleteUsedItems Method With Valid Data");
+		String[] id = {
+			"D9352", "D1030", "D3820"
+		};
+		String itemTable = "Desk";
+		instance.deleteUsedItems(id, itemTable);
+		Connection databaseConnection = null;
+		try {
+			databaseConnection = DriverManager.getConnection("jdbc:mysql://localhost/inventory", USERNAME, PASSWORD);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		boolean result[] = new boolean[3];
+		Statement myStatment = null;
+		ResultSet queryResults = null;
+		for (int i = 0; i<id.length; i++) {
+			try {
+				myStatment = databaseConnection.createStatement();
+				queryResults = myStatment.executeQuery("SELECT * FROM " + itemTable + " WHERE ID = '" + id[i] + "'");
+				result[i] = queryResults.next();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		boolean expResult[] = {
+			false, false, false
+		};
+		assertArrayEquals(expResult, result);
+
+		try {
+			myStatment.execute("INSERT INTO DESK (ID, Type, Legs, Top, Drawer, Price, ManuID) VALUES" +
+				"('D9352',	'Traditional',	'Y',	'N',	'Y',	75,	'002')," +
+				"('D1030',	'Adjustable',	'N',	'Y',	'N',	150,	'002')," +
+				"('D3820',	'Standing',	'Y',	'N',	'N',	150,	'001');");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			myStatment.close();
+			queryResults.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
